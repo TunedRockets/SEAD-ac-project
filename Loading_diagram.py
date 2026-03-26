338cb9be92357724255844839cc404aa9b367a51
RebeccaAEL <r.a.e.loomans@student.tudelft.nl>
2026-03-26 17:45:20 +0100

    weights loading diagram

--- Loading_diagram.py
+++ Loading_diagram.py
@@ -1,2 +1,12 @@
+W_pay = 11966 #kg
+
+n_pas = 100
+W_pas = 92 #kg
+
+W_aft = 1807
+W_forward = 962
+
+W_cargo = n_pas*W_pas + W_aft + W_forward
 
 l_1st_seat = 6.84 # m
+l_2nd_seat = l_1st_seat + 0.9 # 0.9 space per row
