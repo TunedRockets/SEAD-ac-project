4cd1b20fc5a24ac9cc1d0b51eb686641ab34d60e
anwitagadgil <anwitagadgil1@gmail.com>
2026-03-27 14:10:08 +0100

    potato diagram

--- Loading_diagram.py
+++ Loading_diagram.py
@@ -30,18 +30,31 @@ x_both = ((((W_empty * CG) + (W_forward * l_forward) + (W_aft * l_aft)) / (W_emp
 
 
 #Passenger weights and lengths
-l_1 = 6.84 # m
+l_1 = 6.84 - lemac # m
 x_window = []
 w_window = []
-M_n = (W_empty+W_both)*x_both
+x_window_two = []
+w_window_two = []
+M_n = (W_empty+W_both)*x_both 
 # forward window
-for n in range(1,24):
+for n in range(24):
     l_n = l_1 + 0.8*n # 0.8 space per row
     w_n = (W_empty+W_both) + W_pas*2*n
-    M_n =+ 2*W_pas*l_n
-    x_n = (M_n/w_n ) /mac
+    M_n = M_n + 2*W_pas*l_n
+    x_n = (M_n/w_n)
     x_window.append(x_n)
     w_window.append(w_n)
+
+
+M_n = (W_empty+W_both)*x_both 
+l_n = l_n + 0.8
+for n in range(24):
+    l_n = l_n - 0.8 # 0.8 space per row
+    w_n = (W_empty+W_both) + W_pas*2*n
+    M_n = M_n + 2*W_pas*l_n
+    x_n = (M_n/w_n)
+    x_window_two.append(x_n)
+    w_window_two.append(w_n)
 
 
 
@@ -50,4 +63,5 @@ for n in range(1,24):
 
 plt.plot([x_forward, x_empty, x_aft, x_both, x_forward], [W_forward+W_empty, W_empty, W_aft+W_empty, W_both+W_empty, W_forward+W_empty], '-o', color='deeppink')
 plt.scatter(x_window, w_window)
+plt.scatter(x_window_two, w_window_two)
 plt.show()
